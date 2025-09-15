65ff799ecc4e94af05b8c23a36fcfb0764924654
edelpozop <edelpozo@inf.uc3m.es>
2025-09-15 18:41:26 +0200

    1.0 - Added info

--- pre-render.py
+++ pre-render.py
@@ -35,12 +35,8 @@ def pre_render():
                             "text": secondary_email,
                             "href": f"mailto:{secondary_email}"
                         },
-                        {
-                            "text": "<i>Create your resume</i>",
-                            "href": "https://toknow.ai/posts/quarto-resume-template/"
-                        },
                     ]
-                },
+                }
             },
             "format": {
                 "html": {
